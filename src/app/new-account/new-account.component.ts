b62e52b3ceb77743bb8bfb292e87d956be777084
MML8CA <mml8ca@bosch.com>
2020-04-28 12:01:13 -0300

    Injecting the Logging Service into Components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LogginService } from '../Services/loggin.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // Providing the service, so Angular knows how to instanciate
+  providers: [LogginService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  // Injecting the server
+  constructor(private logginService: LogginService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
+
+    // Accessing the service method
+    this.logginService.logStatusChange(accountStatus);
 
   }
 }
